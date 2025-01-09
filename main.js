5d50f5f3626cfe7367a1aebe3e201b0317280901
JemKeHU <staslovel1338@gmail.com>
2025-01-09 18:10:43 +0000

    Made some adjustments

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const myLibrary = [];
 
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const submitButton = document.querySelector("dialog button");
+const myForm = document.querySelector("#book-form");
+const deleteButton = document.createElement("button");
+deleteButton.innerText = "Detele";
+deleteButton.setAttribute("class", "delete-button");
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -15,24 +23,41 @@ function addBookToLibrary(bookObj) {
     myLibrary.push(bookObj);
 }
 
-const bookContainer = document.querySelector("#container");
-const addBook = document.querySelector(".add-book");
-
 function displayLibrary() {
     bookContainer.innerHTML = "";
     myLibrary.forEach(book => {
         const bookElement = document.createElement("div");
+        bookElement.setAttribute("class", "my-book-card");
         bookElement.textContent = book.sayInfo();
         bookContainer.appendChild(bookElement);
+        bookContainer.appendChild(deleteButton)
     });
 }
 
-let book1 = new Book("Miss Shamwoy with magic wand", "James Hedley Chase", 413, "no");
-let book2 = new Book("Blown Creeper", "Katrix", 1, "yes");
-let book3 = new Book("Jam Air", "Chalotte Bromte", 438, "yes");
+myForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
+    const title = formData.get("title");
+    const author = formData.get("author");
+    const pages = parseInt(formData.get("pages"));
+    const read = formData.get("read");
 
-displayLibrary();
+    const newBook = new Book(title, author, pages, read);
+
+    addBookToLibrary(newBook);  
+    displayLibrary();  
+});
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+// "Close" button closes the dialog
+submitButton.addEventListener("click", () => {
+    dialog.close();
+});
+
+const bookContainer = document.querySelector("#container");
+const addBook = document.querySelector(".add-book");
